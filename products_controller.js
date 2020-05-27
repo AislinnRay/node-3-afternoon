5001610ced8935d2fe74cd458afae8f8891756e3
AislinnRay <aislinn.ray.m@gmail.com>
2020-05-27 16:26:55 -0600

    commit 3

--- products_controller.js
+++ products_controller.js
@@ -1,57 +1,62 @@
 module.exports = {
-    create: (req,res) => {
-        const dbInstance = req.app.get('db');
-        const { name, description, price, image_url} = req.body;
+  create: (req, res) => {
+    const dbInstance = req.app.get("db");
+    const { name, description, price, image_url } = req.body;
 
-        dbInstance.create_products([name, description, price, image_url])
-        .then( () => res.sendStatus(200))
-        .catch( err => {
-            res.status(500).send({errorMessage: "create_product did not work"});
-            console.log(err)
-        });
-    },
+    dbInstance
+      .create_products([name, description, price, image_url])
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        res.status(500).send({ errorMessage: "create_product did not work" });
+        console.log(err);
+      });
+  },
 
-    getOne: (req,res) => {
-        const dbInstance = req.app.get('db');
-        const {id} = req.params;
+  getOne: (req, res) => {
+    const dbInstance = req.app.get("db");
+    const { id } = req.params;
 
-        dbInstance.read_product(id)
-        .then( product => res.status(200).send(product))
-        .catch( err => {
-            res.status(500).send({errorMessage: 'read_product did not work'});
-            console.log(err)
-        });
-    },
-    getAll: (req, res) => {
-        const dbInstance = req.app.get('db');
+    dbInstance
+      .read_product(id)
+      .then((product) => res.status(200).send(product))
+      .catch((err) => {
+        res.status(500).send({ errorMessage: "read_product did not work" });
+        console.log(err);
+      });
+  },
+  getAll: (req, res) => {
+    const dbInstance = req.app.get("db");
 
-        dbInstance.read_products()
-        .then( products => res.status(200).send(products))
-        .catch( err => {
-            res.status(500).send({errorMessage: 'read_products did not work'});
-            console.log(err)
-        })
-    },
-    update: (req, res) => {
-        const dbInstance = req.app.get('db');
-        const { params, query } = req;
+    dbInstance
+      .read_products()
+      .then((products) => res.status(200).send(products))
+      .catch((err) => {
+        res.status(500).send({ errorMessage: "read_products did not work" });
+        console.log(err);
+      });
+  },
+  update: (req, res) => {
+    const dbInstance = req.app.get("db");
+    const { params, query } = req;
 
-        dbInstance.update_product([params.id, query.desc])
-        .then( () => res.sendStatus(200))
-        .catch( err => {
-            res.status(500).send({errorMessage: 'update_product did not work'});
-            console.log(err)
-        })
-    },
-    delete: (req, res) => {
-        const dbInstance = req.app.get('db');
-        const { id } = req.params;
+    dbInstance
+      .update_product([params.id, query.desc])
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        res.status(500).send({ errorMessage: "update_product did not work" });
+        console.log(err);
+      });
+  },
+  delete: (req, res) => {
+    const dbInstance = req.app.get("db");
+    const { id } = req.params;
 
-        dbInstance.delete_product(id)
-        .then( () => res.sendStatus(200))
-        .catch( err => {
-            res.status(500).send({errorMessage: 'delete_product did not work'});
-            console.log(err)
-    });
-    }
+    dbInstance
+      .delete_product(id)
+      .then(() => res.sendStatus(200))
+      .catch((err) => {
+        res.status(500).send({ errorMessage: "delete_product did not work" });
+        console.log(err);
+      });
+  },
 };
